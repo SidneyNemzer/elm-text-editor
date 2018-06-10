c7db4873eb79bc366f4d27252657ee8bd5a32eb2
SidneyNemzer <sidney@ucode.com>
2018-06-10 13:45:15 -0700

    Use position record instead of int pair

--- src/Editor/View.elm
+++ src/Editor/View.elm
@@ -4,6 +4,12 @@ import Html exposing (Html, Attribute, text)
 import Html.Attributes as Attribute exposing (class)
 
 
+type alias Position =
+    { line : Int
+    , column : Int
+    }
+
+
 name : String
 name =
     "elm-editor"
@@ -11,30 +17,30 @@ name =
 
 container : List (Attribute msg) -> List (Html msg) -> Html msg
 container attrs =
-    Html.div ([ class <| name ++ "-container" ])
+    Html.div ([ class <| name ++ "-container" ] ++ attrs)
 
 
-line : ( Int, Int ) -> Int -> String -> Html msg
-line ( cursorX, cursorY ) number content =
-    Html.div [ class <| name ++ "-line" ] <|
-        [ Html.span [ class <| name ++ "-line__number" ]
-            [ text <| toString number ]
-        ]
-            ++ if cursorY == number then
-                [ Html.span [] [ text <| String.slice 0 cursorX content ]
-                , Html.span [ class <| name ++ "-line__content--has-cursor" ]
-                    [ text <| String.slice cursorX (cursorX + 1) content
-                    , Html.span [ class <| name ++ "-cursor" ] [ text " " ]
+line : Position -> Int -> String -> Html msg
+line position number rawContent =
+        Html.div [ class <| name ++ "-line" ] <|
+            [ Html.span [ class <| name ++ "-line__number" ]
+                [ text <| toString number ]
+            ]
+                ++ if position.line == number then
+                    [ Html.span [] [ text <| String.slice 0 position.column content ]
+                    , Html.span [ class <| name ++ "-line__content--has-cursor" ]
+                        [ text <| String.slice position.column (position.column + 1) content
+                        , Html.span [ class <| name ++ "-cursor" ] [ text " " ]
+                        ]
+                    , Html.span []
+                        [ text <|
+                            String.slice
+                                (position.column + 1)
+                                (String.length content)
+                                content
+                        ]
                     ]
-                , Html.span []
-                    [ text <|
-                        String.slice
-                            (cursorX + 1)
-                            (String.length content)
-                            content
+                   else
+                    [ Html.span [ class <| name ++ "-line__content" ]
+                        [ text content ]
                     ]
-                ]
-               else
-                [ Html.span [ class <| name ++ "-line__content" ]
-                    [ text content ]
-                ]
