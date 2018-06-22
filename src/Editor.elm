ca398feded4ed7fa4b2080149b1b77e6bbc594b3
SidneyNemzer <sidney@ucode.com>
2018-06-21 23:23:21 -0700

    Expose update function

--- src/Editor.elm
+++ src/Editor.elm
@@ -1,9 +1,13 @@
-module Editor exposing (State, init, view)
+module Editor exposing (State, Msg, init, update, view)
 
 import Html exposing (Html)
 import Editor.Model exposing (InternalState, Position)
-import Editor.Update exposing (Msg)
+import Editor.Update
 import Editor.View
+
+
+type alias Msg =
+    Editor.Update.Msg
 
 
 type State
@@ -20,5 +24,10 @@ init =
 
 
 view : List String -> State -> Html msg
+update : Msg -> State -> ( State, Cmd Msg )
+update msg (State state) =
+    Editor.Update.update msg state |> Tuple.mapFirst State
+
+
 view lines (State state) =
     Editor.View.view lines state
