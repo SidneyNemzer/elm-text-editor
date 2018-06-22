f5530fa8926f3940a85d2b60acbd9694bace4523
SidneyNemzer <sidney@ucode.com>
2018-06-21 23:38:16 -0700

    Use styles from other branch
    fork-Janiczek-elm-editor

--- src/Editor/Styles.elm
+++ src/Editor/Styles.elm
@@ -15,28 +15,44 @@ styleText =
   font-family: monospace;
   border: 1px solid lightgray;
   width: 700px;
+  user-select: none;
+}
+
+.elm-editor-line {
+  cursor: text;
 }
 
 .elm-editor-line__number {
   display: inline-block;
   width: 20px;
   padding-right: 5px;
+  margin-right: 5px;
   text-align: right;
   background-color: lightgray;
+  cursor: default;
 }
 
-.elm-editor-line__content {
-}
-
-.elm-editor-line__content--has-cursor {
+.elm-editor-line__character--has-cursor {
   position: relative;
+}
+
 }
 
 .elm-editor-cursor {
   position: absolute;
   border-left: 1px solid black;
   left: 0;
-  height: 100%
+  height: 100%;
+  animation: blink 0.5s infinite alternate;
+}
+
+@keyframes blink {
+  0% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
 }
 """
 
