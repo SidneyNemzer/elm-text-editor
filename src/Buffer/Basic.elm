c3f50fef675b27aeac8d08511797d1425b4379fe
SidneyNemzer <sidney@ucode.com>
2018-06-21 23:24:41 -0700

    Add insert and listMapAt to Buffer

--- src/Buffer/Basic.elm
+++ src/Buffer/Basic.elm
@@ -1,4 +1,8 @@
-module Buffer.Basic exposing (Buffer, init, lines)
+module Buffer.Basic exposing (Buffer, init, lines, insert)
+
+import List.Extra
+import String.Extra
+import Editor.Model exposing (Position)
 
 
 type Buffer
@@ -8,6 +12,24 @@ type Buffer
 init : String -> Buffer
 init content =
     Buffer content
+
+
+listMapAt : (a -> a) -> Int -> List a -> List a
+listMapAt fn index list =
+    List.Extra.getAt index list
+        |> Maybe.map (\a -> List.Extra.setAt index (fn a) list)
+        |> Maybe.withDefault list
+
+
+insert : Position -> String -> Buffer -> Buffer
+insert { line, column } string (Buffer buffer) =
+    -- I think this could be done more efficiently with a fold; count newlines until
+    -- line, then count characters until column. Insert the char, then do nothing to
+    -- the rest of the buffer
+    String.lines buffer
+        |> listMapAt (String.Extra.insertAt string column) line
+        |> String.join "\n"
+        |> Buffer
 
 
 lines : Buffer -> List String
