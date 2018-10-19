6038b83134598d99494a9ed7129e6627f998e543
Sidney Nemzer <SidneyNemzer@users.noreply.github.com>
2018-10-19 08:16:18 -0400

    refactor: Move State type out of History
    and rename the type to Snapshot

--- src/Editor/Model.elm
+++ src/Editor/Model.elm
@@ -1,7 +1,15 @@
-module Editor.Model exposing (InternalState)
+module Editor.Model exposing (InternalState, Snapshot)
 
+import Buffer exposing (Buffer)
 import Editor.History exposing (History)
 import Position exposing (Position)
+
+
+type alias Snapshot =
+    { cursor : Position
+    , selection : Maybe Position
+    , buffer : Buffer
+    }
 
 
 type alias InternalState =
@@ -9,5 +17,5 @@ type alias InternalState =
     , cursor : Position
     , selection : Maybe Position
     , dragging : Bool
-    , history : History
+    , history : History Snapshot
     }
